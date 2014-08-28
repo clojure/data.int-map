d38b9e2f483a0a659686b3c5c384e8b71ab33aa2
puredanger <alex.miller@cognitect.com>
2014-08-28 16:12:37 -0500

    Split benchmark tests into separate ns so maven build can avoid running them more easily

--- src/test/clojure/clojure/data/int_map_test.clj
+++ src/test/clojure/clojure/data/int_map_test.clj
@@ -6,7 +6,6 @@
     [clojure.core.reducers :as r]
     [clojure.data.int-map :as i]
     [collection-check :as check]
-    [criterium.core :as c]
     [simple-check.generators :as gen]
     [simple-check.properties :as prop]
     [simple-check.clojure-test :as ct :refer (defspec)])
@@ -40,62 +39,6 @@
 
 ;;;
 
-(def n (long 1e6))
-
-(def ordered-entries (vec (map vector (range n) (range n))))
-(def entries (vec (shuffle ordered-entries)))
-
-(deftest ^:benchmark benchmark-maps
-
-  (println "into {} unordered")
-  (c/quick-bench
-    (into {} entries))
-
-  (println "into {} ordered")
-  (c/quick-bench
-    (into {} ordered-entries))
-
-  (println "into (sorted-map) unordered")
-  (c/quick-bench
-    (into (sorted-map) entries))
-
-  (println "into (sorted-map) ordered")
-  (c/quick-bench
-    (into (sorted-map) ordered-entries))
-
-  (println "into (int-map) unordered")
-  (c/quick-bench
-    (into (i/int-map) entries))
-
-  (println "into (int-map) ordered")
-  (c/quick-bench
-    (into (i/int-map) ordered-entries))
-
-  (println "into (int-map) fold/merge unordered")
-  (c/quick-bench
-    (r/fold i/merge conj entries))
-
-  (println "into (int-map) fold/merge ordered")
-  (c/quick-bench
-    (r/fold i/merge conj ordered-entries))
-
-  (let [m (into {} entries)]
-    (println "get {}")
-    (c/quick-bench
-      (get m 1e3)))
-
-  (let [m (into (i/int-map) entries)]
-    (println "get (int-map)")
-    (c/quick-bench
-      (get m 1e3)))
-
-  (let [m (into (sorted-map) entries)]
-    (println "get (sorted-map)")
-    (c/quick-bench
-      (get m 1e3))))
-
-;;;
-
 (defn all-set-algebra-operators-equivalent?
   [generator]
   (prop/for-all [a (gen/vector gen/int) b (gen/vector gen/int)]
@@ -115,36 +58,3 @@
 (defspec prop-dense-all-set-algebra-operators-equivalent 1000
   (all-set-algebra-operators-equivalent? i/dense-int-set))
 
-(deftest ^:benchmark benchmark-modify-set
-  (println "sparse bitset into 1e3")
-  (c/quick-bench
-    (into (i/int-set) (range 1e3)))
-  (println "dense bitset into 1e3")
-  (c/quick-bench
-    (into (i/dense-int-set) (range 1e3)))
-  (println "normal set into 1e3")
-  (c/quick-bench
-    (into #{} (range 1e3)))
-  (println "mutable bitset add 1e3")
-  (c/quick-bench
-    (let [^BitSet bitset (BitSet. 1e3)]
-      (dotimes [idx 1e3]
-        (.set bitset idx true)))))
-
-(deftest ^:benchmark benchmark-check-set
-  (println "check sparse bitset")
-  (let [b (into (i/int-set) (range 1e3))]
-    (c/quick-bench
-      (contains? b 123)))
-  (println "check dense bitset")
-  (let [b (into (i/dense-int-set) (range 1e3))]
-    (c/quick-bench
-      (contains? b 123)))
-  (println "check normal set")
-  (let [s (into #{} (range 1e3))]
-    (c/quick-bench
-      (contains? s 123)))
-  (println "mutable bitset lookup")
-  (let [b (BitSet. 1e3)]
-    (c/quick-bench
-      (.get b 123))))
